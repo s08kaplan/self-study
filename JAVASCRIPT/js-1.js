e4f7e167f80a4d749abe2b4843f78414259aa03b
Serkan <s8ersis@gmail.com>
2023-09-26 07:37:54 +0300

    js-variables

---
+++ JAVASCRIPT/js-1.js
@@ -0,0 +1,84 @@
+// var x = 5;
+// console.log(x);
+
+// {
+//     var x = 3;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+// var x = 7;
+// console.log(x);
+// var x = 10;
+// console.log(x);
+
+// var x = 9;
+// console.log(x);
+
+
+// let x = 2;
+// console.log(x);
+
+// x = 4;
+// console.log(x);
+
+// let x = 4;
+// console.log(x);
+
+// {
+//     let x = 6;
+//     console.log(x);
+// }
+
+
+// console.log(x);
+
+// let x = 5;
+// let y = "5";
+// let z = x + y;
+// console.log(z);
+
+
+// let x = 5;
+// let y = "5";
+// let a = 6;
+// let z = x + y + a;
+// console.log(z);
+
+
+
+// let x = "5";
+// let y = 6;
+// let a = 7;
+// let b = 8;
+// let z = x + y + a + b;
+// console.log(z);
+
+
+// let x = 5;
+// let y = "6";
+// let a = 7;
+// let b = 8;
+// let z = x + y + a + b;
+// console.log(z);
+
+
+// let x = 5;
+// let y = 6;
+// let a = "7";
+// let b = 8;
+// let z = x + y + a + b;
+// console.log(z);
+
+
+let x = 5;
+let y = 6;
+let a = 7;
+let b = "8";
+let z = x + y + a + b;
+console.log(z);
+
+
+
+
