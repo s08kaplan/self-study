d40d4c0963e08f8e8e32a3869185483a6d2e3302
Serkan <s8ersis@gmail.com>
2023-09-28 19:49:37 +0300

    together review

--- JAVASCRIPT/js02/js02.js
+++ JAVASCRIPT/js02/js02.js
@@ -24,6 +24,83 @@
 // ---------------------------------------------------------------
 
 
-let text = "Hello world, welcome to the universe.";
-text = text.startsWith("hello");
-console.log(text);
+// let text = "Hello world, welcome to the universe.";
+// text = text.startsWith("hello");
+// console.log(text);
+
+
+// let myText = "Please locate where 'locate' occurs!";
+
+// let result = myText.lastIndexOf("HEEEEY");
+// console.log(result);
+
+
+// let myText = "Please loCate where 'loCate' occurs!";
+// let result = myText.search("locate");
+// console.log(result);
+//  result = myText.search(/locate/);
+//  console.log(result);
+
+// let a = "Hello My Darling";
+//   a = a.toUpperCase();
+// console.log(a);
+
+// let stat1 = "Hello MY DarlinG";
+// let new2 = stat1.toLowerCase();
+// console.log(new2);
+
+// let sent = "Hello";
+// let sent2 = "JavaScript!";
+// let sent3 = "hello".concat(sent2);
+// console.log(sent3);
+
+
+
+// let text1 = "aaaaaaHello World! hey aaaaaa";
+// let text7 = text1.trimStart();
+// let text8 = text1.trimEnd();
+// // console.log(text1.length);
+// // console.log(text7);
+// // console.log(text7.length);
+// // console.log(text8);
+// // console.log(text8.length);
+// console.log(text1.trim());
+
+
+
+
+
+
+
+
+
+
+// let example = "55555abcds";
+// // let padded = example.padStart(4,"*");
+// let padded2 = example.padEnd(3,"0");
+// // console.log(padded);
+// console.log(padded2);
+
+// let x = 5;
+// x = x.toString();
+// x = x.padStart(3,"*")
+// console.log(x);
+
+
+// let text9 = "HELLO WORLD";
+// // let char = text9.charAt(2);
+// // let c2 = text9.charCodeAt(9);
+// let c3 = text9[10];
+// // console.log(char);
+// // console.log(c2);
+// console.log(c3);
+
+
+
+// let mySent = "n*eve*rthe*less*aha*hey*garanti";
+// let mySent2 = mySent.split("");
+// let mySent3 = mySent.split(",");
+// let mySent4 = mySent.split("*");
+// console.log(mySent2);
+// console.log(mySent3);
+// console.log(mySent4);
