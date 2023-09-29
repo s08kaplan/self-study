9c57c9331ab15105f097563222ee3aeaa206d9c2
Serkan <s8ersis@gmail.com>
2023-09-29 07:36:20 +0300

    arrays-1

---
+++ JAVASCRIPT/JS-Arrays/arrays.js
@@ -0,0 +1,40 @@
+// const students = ["ali", "veli",true,false,1,15,23,"hey"]
+
+// students[0] = "jenny";
+// students[1] = "danny";
+// students[8] = "jimmy";
+
+// // olmayan index'e eleman atamasi yaparsak aralari boş elemanlarla
+// // doldurur
+
+// students[25] = "who";
+// console.log(students);
+
+// console.log(students);
+// console.log(students[3]);
+
+
+// const car = [];
+
+// console.log(typeof car);
+// let car  ;
+
+// car = new Array("sahin","dogan","serce")
+
+// console.log(car);
+// console.log(typeof car);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toString());
+// console.log(typeof fruits);
+
+const myArray = [
+    "ali",
+    1,
+    true,
+    ["hasan","school"],
+    56,
+    [["hey","pay","attention","please",[0]]]
+]
+
+console.log(myArray[3][1]);
