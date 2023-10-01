f096654eaccc521aa10710d4c7727f69de206f86
Serkan <s8ersis@gmail.com>
2023-10-02 01:01:47 +0300

    if-else-switch

---
+++ JAVASCRIPT/JS-IF-ELSE-SWITCH/assignment.js
@@ -0,0 +1,165 @@
+// let x ; 
+// if (x < 0) {
+//     x = "negative number";
+// }else if (x == 0) {
+//     x = 0;
+// }else {
+//     x = "positive number"
+// }
+// console.log(x);
+
+// let x,y,z;
+// console.log("please enter 3 values for x, y and z respectively");
+// x = 5;
+// y= 8;
+// z = 3;
+// let result;
+// if (x<y && y<z ){
+//     z = " z is the largest number";
+//     result = z;
+// }else if (y<x && z<x ) {
+//     x = "x is the largest number";
+//     result = x;
+// } else {
+//    y = "y is the largest number"
+//    result = y
+// }
+
+// console.log(result);
+
+// let x,y;
+// let result;
+// let symbol ;
+// x = 3;
+// y = 4;
+// symbol = "/"
+// if (symbol == "+") {
+//     result = x + y;
+// }else if (symbol == "-") {
+//     result = x - y;
+// }else if (symbol == "*"){
+//     result = x * y
+// }else if (symbol == "/") {
+//    result = (x/y)
+// }
+ 
+// console.log(result);
+// ----------------------------------
+
+
+
+
+
+
+// let x = prompt("enter a number : ") 
+// if (x < 0) {
+//     x = "the given number is a negative number";
+// }else if (x == 0) {
+//     x = "the given number is neither negative nor positive it is 0";
+// }else {
+//     x = "the given number is a positive number"
+// }
+// console.log(x);
+
+
+
+
+
+
+
+// let x = prompt("enter a number : ")
+// let y = prompt("enter a number : ")
+// let z = prompt("enter a number : ")
+
+
+
+// let result;
+// if (x<y && y<z ){
+//     z = " z is the largest number";
+//     result = z;
+// }else if (y<x && z<x ) {
+//     x = "x is the largest number";
+//     result = x;
+// } else {
+//    y = "y is the largest number"
+//    result = y
+// }
+
+// console.log(result);
+
+
+
+
+
+
+
+// let x = prompt("enter a number : ");
+// let y = prompt("enter another number :");
+// let symbol = prompt("choose one of the operation : '+', '-', '*', '/'")
+// let result;
+
+// if (symbol == "+") {
+//     result = x + y;
+// }else if (symbol == "-") {
+//     result = x - y;
+// }else if (symbol == "*"){
+//     result = x * y
+// }else if (symbol == "/") {
+//    result = (x/y)
+// }
+ 
+// console.log(result);
+
+
+// let day = prompt(`please enter a day from 1 to 7:`);
+// let message;
+// switch(parseInt(day)){
+//     case 1:
+//         message = "you have InClass";
+//         break;
+//     case 2:
+//         message = "you have InClass";
+//         break; 
+//     case 3:
+//         message = "you have InClass";
+//         break;
+//     case 4:
+//         message = "you have InClass";
+//         break;    
+//     case 5:
+//         message = "you have TEamwork";
+//         break;
+//     case 6:
+//         message = "you have InClass and Workshop";
+//         break;
+//     case 7:
+//         message = "you have self study";
+//         break;
+//     default:
+//         alert("please enter a valid day number");
+//         break;               
+// }
+
+// console.log(message);
+
+
+
+
+
+// const grade = prompt("enter your grade : ");
+
+// const message = parseInt(grade) >=50 ? "you passed" : "sorry keep studying";
+// console.log(message);
+
+
+// const note = prompt("please enter your grade : ");
+// let result;
+// if (note < 40){
+//     result = "you failed";
+// }else if(note >=40 && note<= 50) {
+//     result = " your pass is on a condition 'it depends on your theacher:)))'"
+// }else {
+//     result = "you passed"
+// }
+
+// console.log(result);
