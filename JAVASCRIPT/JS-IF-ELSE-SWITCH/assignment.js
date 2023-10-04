8ee2be73b0ffa158134c27b8c94c72a81ccfe1ee
Serkan <s8ersis@gmail.com>
2023-10-04 07:45:46 +0300

    solutions corrected

--- JAVASCRIPT/JS-IF-ELSE-SWITCH/assignment.js
+++ JAVASCRIPT/JS-IF-ELSE-SWITCH/assignment.js
@@ -256,3 +256,87 @@
 // let fahrenheitToCElsius = parseInt(prompt("enter your fahrenheit value :"));
 // fahrenheitToCElsius = (fahrenheitToCElsius - 32) * 5/9
 // console.log(fahrenheitToCElsius);
+
+
+
+
+
+// =========================!!!!!!!==================
+
+
+// const x = Number(prompt())
+// const y = Number(prompt())
+// const z = Number(prompt())
+
+// const sumNum = x + y + z
+// const productNum = x * y * z
+
+// let message = ""
+
+// if ( x < y && y < z) {
+//     message = `min value is x: ${x} and max value is z : ${z}
+//     the sum is : ${sumNum} and the product is : ${productNum}`
+// }else if (x < y && x < z && z < y){
+//     message = `min value is x: ${x} and max value is y : ${y}
+//     the sum is : ${sumNum} and the product is : ${productNum}`
+// }else if (y < x && x < z){
+//     message = `min value is y: ${y} and max value is z : ${z}
+//     the sum is : ${sumNum} and the product is : ${productNum}`
+// }else if (y < x && z < x){
+//     message = `min value is y: ${y} and max value is x : ${x}
+//     the sum is : ${sumNum} and the product is : ${productNum}`
+// }else if (z < x && x < y){
+//     message = `min value is x: ${x} and max value is z : ${z}
+//     the sum is : ${sumNum} and the product is : ${productNum}`
+// }
+
+// ====================================================
+
+// const day = Number(prompt("Please enter a number of day from 1 to 31(included):"))
+// const month = Number(prompt("Please enter a number of month from 1 to 12(included):"))
+
+// let message = ""
+
+// if ( day < 1 || day > 31 || month < 1 || month >12) {
+//     message = "Please enter a valid value!"
+// }else if (day >= 21 && month ==2 || day <= 31 && month == 5) {
+//     message = `The season for ${month}/${day} is Spring`
+// }else if (day >= 1 && month == 6 || day <=21 && month == 9) {
+//     message = `The season for ${month}/${day} is Summer`
+// }else if (day >= 22 && month ==9 || day <= 20 && month == 12 ) {
+//     message = `The season for ${month}/${day} is Fall`
+// }else {
+//     message = `The season for ${month}/${day} is Winter`
+// }
+
+// console.log(message);
+
+
+
+// ============================
+
+
+// const day = Number(prompt("Please enter a number of day from 1 to 31(included):"))
+// const month = Number(prompt("Please enter a number of month from 1 to 12(included):"))
+
+// let message = ""
+
+// switch( true ){
+//     case day >= 21 && month ==2 || day <= 31 && month == 5:
+//         message = `The season for ${month}/${day} is Spring`;
+//     break;
+//     case day >= 1 && month == 6 || day <=21 && month == 9:
+//         message = `The season for ${month}/${day} is Summer`;
+//     break;
+//     case day >= 22 && month ==9 || day <= 20 && month == 12 :
+//         message = `The season for ${month}/${day} is Fall`;
+//         break;
+//     case day>=21 && month == 12 || day <= 20 && month ==2:
+//         message = `The season for ${month}/${day} is Winter`;
+//         break;
+//     case  day < 1 || day > 31 && month < 1 || month >12:
+//         message = "Please enter a valid value!";
+//         break;
+// }
+   
+// console.log(message);
