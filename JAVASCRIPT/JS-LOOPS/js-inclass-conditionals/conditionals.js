b40db7221b5db4c0fe39dc5d635925e70a11281b
Serkan <s8ersis@gmail.com>
2023-10-05 23:17:29 +0300

    exercises for loops

---
+++ JAVASCRIPT/JS-LOOPS/js-inclass-conditionals/conditionals.js
@@ -0,0 +1,75 @@
+// const x = Number(prompt())
+// const y = Number(prompt())
+// const z = Number(prompt())
+
+// let message = (x > y && x > z)?"x en buyuk":((y > x && y >z)?"y en buyuk": "z en buyuk")
+
+// console.log(message);
+
+
+
+// const x = 5
+// const y = 3
+// const z = 7
+
+// let message = (x > y && x > z)?"x en buyuk":(y > x && y >z)?"y en buyuk": "z en buyuk"
+
+// console.log(message);
+
+
+
+// const note = 50
+// const result = note > 50?"passed":"failed"
+// console.log(result);
+
+
+// const hizmetYili = 5
+// let maas = 15000
+// hizmetYili >= 5 ? (maas*=1.1) : ""
+// console.log(maas);
+
+
+// let x = 1;
+// let result = 1;
+// let i = 1;
+
+// if (x < 0) {
+//     console.log("Please enter a positive number");
+// }else if(x === 1){
+//     console.log(1);
+// } else {
+//     while (i <= x) {
+//         result *= i;
+//         i++;
+//     }
+    
+// }
+
+// console.log(result);
+
+
+
+// const count = Number(prompt("please enter how many random numbers you want:"))
+// const count = 5
+
+// for (let i = 1;i <= count; i++){
+//     const randomNumber = Math.round(Math.random()*100);
+//     console.log(`${i}. random number : ${randomNumber}`);
+// }
+//  const x = Number(prompt())
+//  const y = Number(prompt())
+// const x = 5;
+// const y = 10;
+// let result = 0;
+
+// if (x < y) {
+//     for (let i = x; i <= y; i++) {
+//         result += i;
+//     }
+// } else {
+//     for (let i = y; i <= x; i++) {
+//         result += i;
+//     }
+// }
+
+// console.log(result);
