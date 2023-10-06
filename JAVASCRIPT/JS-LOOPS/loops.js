1e870914ce1e7ec7e1142308d123a12a80e899ae
Serkan <s8ersis@gmail.com>
2023-10-06 21:45:48 +0300

    solutions checked

--- JAVASCRIPT/JS-LOOPS/loops.js
+++ JAVASCRIPT/JS-LOOPS/loops.js
@@ -93,39 +93,123 @@
 //   console.log(row);
 // }
 
-
 // ====================================================
 
+// function trafficLightSimulation() {
+//     // Set the durations for each light phase (in milliseconds)
+//     const greenLightDuration = 5000; // 5 seconds
+//     const yellowLightDuration = 2000; // 2 seconds
+//     const redLightDuration = 3000; // 3 seconds
+//
+//     // Function to display a message and change the light color
+//     function showMessageAndChangeLight(message, color) {
+//         console.log(message);
+//         document.body.style.backgroundColor = color;
+//     }
 
-function trafficLightSimulation() {
-    // Set the durations for each light phase (in milliseconds)
-    const greenLightDuration = 5000; // 5 seconds
-    const yellowLightDuration = 2000; // 2 seconds
-    const redLightDuration = 3000; // 3 seconds
-    const total = 60000
-    
-    // Function to display a message and change the light color
-    function showMessageAndChangeLight(message, color) {
-        console.log(message);
-        document.body.style.backgroundColor = color;
-    }
+//     // Green light phase
+//     showMessageAndChangeLight("WAIT for green light", "red");
+//     setTimeout(function () {
+//         // After green light duration, change to yellow
+//         showMessageAndChangeLight("READY for yellow light", "yellow");
 
-    // Green light phase
-    showMessageAndChangeLight("WAIT for green light", "red");
-    setTimeout(function () {
-        // After green light duration, change to yellow
-        showMessageAndChangeLight("READY for yellow light", "yellow");
+//         // Yellow light phase
+//         setTimeout(function () {
+//             // After yellow light duration, change to red
+//             showMessageAndChangeLight("GO GO GO for green light", "green");
 
-        // Yellow light phase
-        setTimeout(function () {
-            // After yellow light duration, change to red
-            showMessageAndChangeLight("GO GO GO for green light", "green");
+//             // Red light phase (restart the simulation)
+//             setTimeout(trafficLightSimulation, redLightDuration);
+//         }, yellowLightDuration);
+//     }, greenLightDuration);
+// }
 
-            // Red light phase (restart the simulation)
-            setTimeout(trafficLightSimulation, redLightDuration);
-        }, yellowLightDuration);
-    }, greenLightDuration);
-}
+// // Start the traffic light simulation
+// trafficLightSimulation();
 
-// Start the traffic light simulation
-trafficLightSimulation();
+// console.log("you can order : pizza,hamburger,coke,soda");
+
+// const userMeal = prompt("please give your order for meal :");
+// const userDrink = prompt("please give your order for drink :");
+// let message = "";
+// switch (userMeal && userDrink) {
+//   case "pizza" && "coke":
+//     message = "bon appetite";
+//     break;
+//   case "pizza" && "soda":
+//     message = "good choice";
+//     break;
+//   case "hamburger" && "coke":
+//     message = "come again to have delicious take away";
+//     break;
+//     case "hamburger"&& "soda":
+//         message = "see you again;
+//         break;
+//     default :
+//     message = "sorry we don't have your order"
+//     break;
+// }
+
+// const userNum = Number(prompt("enter a number :"))
+
+// let message = ""
+
+// if ( userNum % 15 === 0) {
+//              message = "WONDERFUL"
+// }else if (userNum % 3 === 0) {
+//     message = "HEY"
+// }else if (userNum % 5 === 0) {
+//     message = "OLEY"
+// }else {
+//     message = `${userNum}`
+// }
+
+
+
+
+// let message = "";
+
+// for (let userNum = 1; userNum <= 100; userNum++) {
+//   if (userNum % 15 === 0) {
+//     message = "WONDERFUL";
+//   } else if (userNum % 3 === 0) {
+//     message = "HEY";
+//   } else if (userNum % 5 === 0) {
+//     message = "OLEY";
+//   } else {
+//     message = `${userNum}`;
+//   }
+//   console.log(message);
+// }
+
+
+
+// =========================================================
+
+
+// const userNum = Number(prompt("enter a number :"))
+// let message = ""
+// let total = 1
+// if ( userNum < 0){
+//     message = "enter a positive number"
+//     console.log(message);
+// }else if(userNum === 0){
+//     message = 1
+//     console.log(message);
+// }else {
+//     for (let i = 1;i <= userNum;i++){
+//         total *=i
+//     }console.log(total);
+// }
+// ==================================================================
+
+
+// const userNum = Number(prompt("enter a number :"))
+// let message =""
+// for (let i= 1; i <= 100;i++){
+//     if (i % 2 ==1) {
+//         message = `number is : ${i} and double of number is :${i*2} `
+//     }else if (i % 2 == 0){
+//         message = `number is : ${i} and square of number is :${i**2} `
+//     }console.log(message);
+// }
