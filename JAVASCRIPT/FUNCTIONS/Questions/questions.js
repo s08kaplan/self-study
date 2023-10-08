5f15d7faac05c71140c7559d769bd4e0dc4bbc37
Serkan <s8ersis@gmail.com>
2023-10-08 07:10:11 +0300

    hw done

---
+++ JAVASCRIPT/FUNCTIONS/Questions/questions.js
@@ -0,0 +1,59 @@
+// function reversedText(text){
+//     let reversed = "";
+//     let words = "";
+//     for (let word of text){
+//         words = text.split(" ")
+//     }for (let i = 0;i <= words.length-1;i++){
+//         reversed += words[(words.length-1)-i] + " "
+//     }
+//     return reversed
+// } 
+// const text = "hello js world 2023";
+// console.log(reversedText(text));
+
+
+
+// function reversedText(text) {
+//     let reversed = "";
+//     let words = text.split(" ");
+  
+//     for (let i = words.length - 1; i >= 0; i--) {
+//       reversed += words[i] + " ";
+//     }
+  
+//     return reversed.trim(); // Remove the trailing space and return the reversed text
+//   }
+  
+//   const text = "hello js world 2023";
+//   console.log(reversedText(text));
+ 
+
+
+// function reversed(sentence){
+//     let reversedText = ""
+//    const n = (sentence.length) 
+//    for (let i = 1; i >= 0 && i <= n; i++ ){
+//     reversedText += sentence[n-i];
+//    }
+//    return reversedText;
+// }
+
+// const text = "hey let's get started"
+// console.log(reversed(text));
+
+
+
+
+
+// function reversedText(sentence){
+//     let words = ""
+//     let reversed = ""
+//     words = sentence.split(" ")
+//     for (let i = words.length - 1;i >=0;i--){
+//         reversed += words[i] + " "
+//     }
+//     return reversed;
+// } 
+
+// let text = "Hello Js World 2023";
+// console.log(reversedText(text));
