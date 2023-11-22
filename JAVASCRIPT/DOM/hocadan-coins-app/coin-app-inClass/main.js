eb3e4bd6fd9723dccefe18c89bc33c0ae521aba3
Serkan <s8ersis@gmail.com>
2023-11-22 23:42:31 +0300

    to review and improve

---
+++ JAVASCRIPT/DOM/hocadan-coins-app/coin-app-inClass/main.js
@@ -0,0 +1,22 @@
+//? Projenin style dosyasini ana js (main.js) kodumuza import ettik.
+import "./scss/style.scss"
+import { apiRequest } from "./src/apiRequest.js"
+
+//? Selector
+const form = document.querySelector("header form")
+
+//? Form için submit event'nin tanimlanmasi
+form.addEventListener("submit", (e) => {
+  e.preventDefault() //? tüm submit davranıslarini engelle
+  getCoinData() //? API'ye istek at.
+  e.target.reset() //? formu silme davranısi calistir
+})
+
+const getCoinData = () => {
+  const input = document.querySelector("header form input").value
+  if (!input.trim()) {
+    alert("Input can not be blank ")
+  } else {
+    apiRequest(input)
+  }
+}
